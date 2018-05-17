17f7385e81670ff7dc6341dde1c687603e2e545f
Nicholas Canzoneri <canzoneri.nicholas@gmail.com>
2018-05-17 15:43:20 -0400

    Adjust columns

--- elasticsearch-settings/es-settings.js
+++ elasticsearch-settings/es-settings.js
@@ -8,10 +8,10 @@ var App = (function () {
   self.generateTableRow = function (setting) {
     var name = setting.name || setting.raw_name;
     var cells = `
-      <td class="setting-name">${(name.length < 50) ? name : "<abbr title=\"" + self.htmlEncode(name) + "\">" + name.substring(0, 50) + "&hellip;</abbr>" }</td>
+      <td><a href="#${name}" title="Permalink">🔗</a></td>
       <td><a target="_blank" href="https://github.com/elastic/elasticsearch/blob/v6.2.4/${setting.code_file}#L${setting.code_line}"><img style="height:32px;" src="/images/github-icon.png" alt="See code definition" /></a></td>
       <td><a target="_blank" href="https://www.elastic.co/search?q=${name}&section=Learn%2FDocs%2FElasticsearch%2FReference%2F6.2&tags=Elasticsearch"><img style="height:32px" src="/images/elasticsearch-icon.png" alt="See documentation" /></a></td>
-      <td><a href="#${name}" title="Permalink">🔗</a></td>
+      <td class="setting-name">${(name.length < 40) ? name : "<abbr title=\"" + self.htmlEncode(name) + "\">" + name.substring(0, 40) + "&hellip;</abbr>" }</td>
       <td>${(setting.properties || []).join(", ")}</td>
       <td>${setting.java_type}</td>
       <td>${(setting.default_arg.length < 40) ? setting.default_arg : "<abbr title=\"" + self.htmlEncode(setting.default_arg) + "\">" + setting.default_arg.substring(0, 40) + "&hellip;</abbr>"}</td>`;
