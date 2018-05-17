3e1177da352821106cd90b090580accf42788f3c
Nicholas Canzoneri <canzoneri.nicholas@gmail.com>
2018-05-17 13:15:58 -0400

    Truncate default arg column more

--- elasticsearch-settings/es-settings.js
+++ elasticsearch-settings/es-settings.js
@@ -7,7 +7,7 @@ var App = (function () {
       <td><a href="#${name}" title="Permalink">🔗</a></td>
       <td class="setting-name">${(name.length < 60) ? name : "<abbr title=\"" + name + "\">" + name.substring(0, 60) + "&hellip;</abbr>" }</td>
       <td>${setting["JavaType"]}</td>
-      <td>${(setting["DefaultArg"].length < 60) ? setting["DefaultArg"] : "<abbr title=\"" + setting["DefaultArg"] + "\">" + setting["DefaultArg"].substring(0, 60) + "&hellip;</abbr>"}</td>
+      <td>${(setting["DefaultArg"].length < 40) ? setting["DefaultArg"] : "<abbr title=\"" + setting["DefaultArg"] + "\">" + setting["DefaultArg"].substring(0, 40) + "&hellip;</abbr>"}</td>
       <td>${(setting["Properties"] || []).join(", ")}</td>
       <td><a target="_blank" href="https://github.com/elastic/elasticsearch/blob/master/${setting["CodeFile"]}#L${setting["CodeLine"]}"><img style="height:32px;" src="/images/github-icon.png" alt="See code definition" /></a></td>
       <td><a target="_blank" href="https://www.elastic.co/search?q=${name}&section=Learn%2FDocs%2FElasticsearch%2FReference%2F6.2&tags=Elasticsearch"><img style="height:32px" src="/images/elasticsearch-icon.png" alt="See documentation" /></a></td>`;
