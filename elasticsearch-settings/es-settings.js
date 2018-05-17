98c1ef0841dc95532b557657ff2e0bfb26aea49a
Nicholas Canzoneri <canzoneri.nicholas@gmail.com>
2018-05-16 22:49:03 -0400

    Add more information to the details

--- elasticsearch-settings/es-settings.js
+++ elasticsearch-settings/es-settings.js
@@ -7,7 +7,33 @@ var App = (function () {
     name.appendChild(document.createTextNode(setting["Name"]));
 
     var details = document.createElement("p");
-    details.appendChild(document.createTextNode("more info here"));
+
+    var infoList = document.createElement("ul");
+
+    var typeElem = document.createElement("li");
+    typeElem.textContent = `Java type: ${settings["JavaType"]}`;
+
+    var defaultElem = document.createElement("li");
+    defaultElem.textContent = `Default value: ${setings["DefaultArg"]}`;
+
+    var propertiesElem = document.createElement("li");
+    propertiesElem.textContent = `Properties: ${settings["Properties"].join(", ")}`
+
+    var githubLink = document.createElement("a");
+    githubLink.href = `https://github.com/elastic/elasticsearch/blob/master/${setting["CodeFile"]}#L${setting["CodeLine"]}`;
+    githubLink.text = `Look at source for ${settings["Name"]}`;
+
+    var docsLink = document.createElement('a");
+    docsLink.href = `https://www.elastic.co/search?q=${settings["Name"]}&section=Learn%2FDocs%2FElasticsearch%2FReference%2F6.2&tags=Elasticsearch`;
+    docsLink.text = `Search docs for ${settings["Name"]}`;
+
+    infoList.appendChild(typeElem);
+    infoList.appendChild(defaultElem);
+    infoList.appendChild(propertiesElem);
+    infoList.appendChild(document.createElement("li").appendChild(githubLink));
+    infoList.appendChild(document.createElement("li").appendChild(docsLink));
+
+    details.appendChild(infoList);
 
     root.appendChild(name);
     root.appendChild(details);
@@ -17,9 +43,12 @@ var App = (function () {
 
   self.init = function () {
     var container = document.getElementById("settings-list");
-    ES_SETTINGS.map(function (setting) {
-      container.appendChild(self.generateDetails(setting));
+    ES_SETTINGS.sort(function (a, b) {
+      return a["Name"].localeCompare(b["Name"]);
     })
+    .map(function (setting) {
+      container.appendChild(self.generateDetails(setting));
+    });
   };
 
   return self;
